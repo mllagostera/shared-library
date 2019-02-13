92017f14b095b92f4eceb593bdadde445de9eb36
Miquel <36759597+mllagostera@users.noreply.github.com>
2019-02-13 17:32:41 +0100

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,16 +1,20 @@
 #!/usr/bin/env groovy
 
-def call(String buildResult) {
-  if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
-  }
-  else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
-  }
-  else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
-  }
-  else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
-  }
+def call(String buildStatus = 'STARTED', time) {
+    // Build status of null means success.
+    buildStatus = buildStatus ?: 'SUCCESS'
+
+    def color
+    def message
+    color = 'good'
+    message = 'Succes after'
+    if (buildStatus == 'UNSTABLE') {
+        color = 'warning'
+        message = 'Still Failing after'
+    } else if (buildStatus == 'FAILURE') {
+        color = 'danger'
+        message = 'Failure after'
+    }
+    def msg = "${env.JOB_NAME} - ${env.BUILD_DISPLAY_NAME} " + message + " " + time + " (<${env.BUILD_URL}|Open>)";
+    slackSend(color: color, message: msg)
 }
